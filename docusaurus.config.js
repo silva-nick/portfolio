a4d4144dc52537dd8473dc57429cd1772d119fc4
silva-nick <silva.d.nicholas@gmail.com>
2021-06-29 18:03:02 -0500

    add prettier config and apply to docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,71 +1,71 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: "My Portfolio",
-  tagline: "A React-based static github website template.",
-  url: "https://silva-nick.github.io",
-  baseUrl: "/portfolio/",
-  organizationName: "silva-nick", // Usually your GitHub org/user name.
-  projectName: "portfolio", // Usually your repo name.
-  onBrokenLinks: "throw",
-  trailingSlash: "true",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
+  title: 'My Portfolio',
+  tagline: 'A React-based static github website template.',
+  url: 'https://silva-nick.github.io',
+  baseUrl: '/portfolio/',
+  organizationName: 'silva-nick', // Usually your GitHub org/user name.
+  projectName: 'portfolio', // Usually your repo name.
+  onBrokenLinks: 'throw',
+  trailingSlash: 'true',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
   themeConfig: {
     navbar: {
-      title: "Home",
+      title: 'Home',
       logo: {
-        alt: "My Logo",
-        src: "img/logo.svg",
+        alt: 'My Logo',
+        src: 'img/logo.svg',
       },
       items: [
         {
-          type: "doc",
-          docId: "about",
-          position: "left",
-          label: "More Info",
+          type: 'doc',
+          docId: 'about',
+          position: 'left',
+          label: 'More Info',
         },
-        { to: "blog", label: "Blog", position: "left" },
+        { to: 'blog', label: 'Blog', position: 'left' },
         {
-          href: "https://github.com/silva-nick",
-          label: "GitHub",
-          position: "right",
+          href: 'https://github.com/silva-nick',
+          label: 'GitHub',
+          position: 'right',
         },
       ],
     },
     footer: {
-      style: "dark",
+      style: 'dark',
       links: [
         {
-          title: "silva-nick",
+          title: 'silva-nick',
           items: [
             {
-              label: "Home",
-              to: "/",
+              label: 'Home',
+              to: '/',
             },
             {
-              label: "Resume",
-              to: "/docs/resume",
+              label: 'Resume',
+              to: '/docs/resume',
             },
             {
-              label: "GitHub",
-              to: "https://github.com/<GITHUB-USER>",
+              label: 'GitHub',
+              to: 'https://github.com/<GITHUB-USER>',
             },
           ],
         },
         {
-          title: "About",
+          title: 'About',
           items: [
             {
-              label: "About me",
-              to: "/docs/about",
+              label: 'About me',
+              to: '/docs/about',
             },
             {
-              label: "Timeline",
-              to: "/docs/timeline",
+              label: 'Timeline',
+              to: '/docs/timeline',
             },
             {
-              label: "Blog",
-              to: "/blog",
+              label: 'Blog',
+              to: '/blog',
             },
           ],
         },
@@ -74,20 +74,20 @@ module.exports = {
     },
   },
 
-  themes: ["docusaurus-portfolio-theme"],
+  themes: ['docusaurus-portfolio-theme'],
   plugins: [
     [
-      "docusaurus-portfolio-plugin",
+      'docusaurus-portfolio-plugin',
       {
-        username: "silva-nick",
-        path: "/",
-        pageTitle: "My Site",
-        pageDescription: "About me.",
+        username: 'silva-nick',
+        path: '/',
+        pageTitle: 'My Site',
+        pageDescription: 'About me.',
         userOptions: {},
         repoOptions: {
-          type: "all",
-          sort: "updated",
-          direction: "desc",
+          type: 'all',
+          sort: 'updated',
+          direction: 'desc',
           numRepos: 10,
         },
       },
@@ -96,22 +96,22 @@ module.exports = {
 
   presets: [
     [
-      "@docusaurus/preset-classic",
+      '@docusaurus/preset-classic',
       {
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            "https://github.com/facebook/docusaurus/edit/master/website/",
+            'https://github.com/facebook/docusaurus/edit/master/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            "https://github.com/facebook/docusaurus/edit/master/website/blog/",
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: require.resolve('./src/css/custom.css'),
         },
       },
     ],
