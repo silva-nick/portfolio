126f1df4f711fa9adc87cec248ab9ee5860d9736
silva-nick <silva.d.nicholas@gmail.com>
2021-06-29 17:50:17 -0500

    initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,118 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'My Portfolio',
+  tagline: 'A React-based static github website template.',
+  url: 'https://github.com',
+  baseUrl: '/',
+  organizationName: 'silva-nick', // Usually your GitHub org/user name.
+  projectName: '', // Usually your repo name.
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  themeConfig: {
+    navbar: {
+      title: 'Home',
+      logo: {
+        alt: 'My Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'doc',
+          docId: 'about',
+          position: 'left',
+          label: 'More Info',
+        },
+        { to: 'blog', label: 'Blog', position: 'left' },
+        {
+          href: 'https://github.com/silva-nick',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'silva-nick',
+          items: [
+            {
+              label: 'Home',
+              to: '/',
+            },
+            {
+              label: 'Resume',
+              to: '/docs/resume',
+            },
+            {
+              label: 'GitHub',
+              to: 'https://github.com/<GITHUB-USER>',
+            },
+          ],
+        },
+        {
+          title: 'About',
+          items: [
+            {
+              label: 'About me',
+              to: '/docs/about',
+            },
+            {
+              label: 'Timeline',
+              to: '/docs/timeline',
+            },
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+          ],
+        },
+      ],
+      // copyright: `Copyright © ${new Date().getFullYear()} MyPortfolio. Built with Docusaurus.`,
+    },
+  },
+
+  themes: ['docusaurus-portfolio-theme'],
+  plugins: [
+    [
+      'docusaurus-portfolio-plugin',
+      {
+        username: 'silva-nick',
+        path: '/',
+        pageTitle: 'My Site',
+        pageDescription: 'About me.',
+        userOptions: {},
+        repoOptions: {
+          type: 'all',
+          sort: 'updated',
+          direction: 'desc',
+          numRepos: 10,
+        },
+      },
+    ],
+  ],
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
